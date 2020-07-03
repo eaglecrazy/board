c49c72439266f00f1de6a3bc4ff5b0925b93c766
eaglecrazy <munoyoo3munoyoo3>
2020-07-03 15:09:40 +0300

    Доделал хлебные крошки

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -11,7 +11,22 @@ Breadcrumbs::register('login', function (BreadcrumbsGenerator $crumbs){
     $crumbs->push('Login', route('login'));
 });
 
+Breadcrumbs::register('register', function (BreadcrumbsGenerator $crumbs){
+    $crumbs->parent('home');
+    $crumbs->push('Register', route('register'));
+});
+
+Breadcrumbs::register('cabinet', function (BreadcrumbsGenerator $crumbs){
+    $crumbs->parent('home');
+    $crumbs->push('Cabinet', route('cabinet'));
+});
+
 Breadcrumbs::register('password.request', function (BreadcrumbsGenerator $crumbs){
     $crumbs->parent('login');
     $crumbs->push('Input email', route('password.request'));
+});
+
+Breadcrumbs::register('password.reset', function (BreadcrumbsGenerator $crumbs){
+    $crumbs->parent('password.request');
+    $crumbs->push('Change', route('password.reset'));
 });
