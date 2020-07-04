28070d602db67cbcc2f69b8fa9c2e17f378f5d28
eaglecrazy <munoyoo3munoyoo3>
2020-07-04 19:47:28 +0300

    Создал группу роутов admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -7,13 +7,8 @@ use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
-        return view('cabinet.home');
+        return view('admin.home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,19 @@
 <?php
 
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
-
 Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
 
-//Route::get('/register', 'Auth\RegisterController@form')->name('register');
-//Route::post('/register', 'Auth\RegisterController@register');
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.',
+    'namespace' => 'Admin',
+    'middleware' => ['auth'],
+], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('users', 'UserController');
+});
