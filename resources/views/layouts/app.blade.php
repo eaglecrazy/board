adc736f4c05b653ba4cb6be74e749edf5b9991db
eaglecrazy <munoyoo3munoyoo3>
2020-07-04 19:56:47 +0300

    Добавил вьюху админки и сделал ссылки в шаблоне

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,9 @@
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('admin.home') }}">Admin</a>
+                                <a class="dropdown-item" href="{{ route('cabinet') }}">Cabinet</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
