e298303932ff4b1b380637df220c7312af4bbaa8
eaglecrazy <munoyoo3munoyoo3>
2020-07-04 14:05:49 +0300

    Работаю с LoginController.php (3.50)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,28 +5,41 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth;
 
 class LoginController extends Controller
 {
-    use AuthenticatesUsers;
+    use ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/cabinet';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if($this->hasTooManyLoginAttempts($request)){
+            $this->fireLockoutEvent($request);
+            $this->sendLockoutResponse($request);
+        }
+
+        $authentificate
+    }
+
 
     public function authenticated(Request $request, $user)
     {
