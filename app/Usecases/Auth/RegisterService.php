183063bf1a0db5d733a92003358c3ef67b254bd3
eaglecrazy <munoyoo3munoyoo3>
2020-07-06 18:42:27 +0300

    Добавил внедрение зависимостей в RegisterService

--- app/Usecases/Auth/RegisterService.php
+++ app/Usecases/Auth/RegisterService.php
@@ -7,15 +7,26 @@ use App\Entity\User;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Mail\Auth\VerifyMail;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Events\Dispatcher as DispatcherInterface;
+use Illuminate\Contracts\Mail\Mailer as MailerInterface;
 use Illuminate\Support\Facades\Mail;
 
 class RegisterService
 {
+    private $mailer;
+    private $dispatcher;
+
+    public function __construct(MailerInterface $mailer, DispatcherInterface $dispatcher)
+    {
+        $this->dispatcher = $dispatcher;
+        $this->mailer = $mailer;
+    }
+
     public function register(RegisterRequest $request): void
     {
         $user = User::register($request['name'], $request['email'], $request['password']);
-//        Mail::to($user->email)->send(new VerifyMail($user));
-        event(new Registered($user));
+//        $this->mailer->to($user->email)->send(new VerifyMail($user));
+        $this->dispatcher->dispatch(new Registered($user));
     }
 
     public function verify($id): void
