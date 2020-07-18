5b7cbcb20708a3b0b98c526516e0d3b29eb6c951
eaglecrazy <munoyoo3munoyoo3>
2020-07-18 18:57:24 +0300

    Добавил команду ChangeRole

---
+++ app/Console/Commands/User/RoleCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands\User;
+
+use Illuminate\Console\Command;
+use App\Entity\User;
+
+class RoleCommand extends Command
+{
+    protected $signature = 'user:role {email} {role}';
+
+    protected $description = 'Ser role for user';
+
+    public function handle() : bool
+    {
+        $email = $this->argument('email');
+        $role = $this->argument('role');
+
+        $user = User::where('email', $email)->first();
+        if(!$user){
+            $this->error('Undefined user with email ' . $email);
+            return false;
+        }
+
+        try {
+            $user->changeRole($role);
+        } catch (\DomainException $e){
+            $this->error($e->getMessage());
+            return false;
+        }
+
+        $this->info('Role successfully changed');
+        return true;
+    }
+}
