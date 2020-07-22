67aae6748190ca1a47be0b68aea88ae14a5c3a2e
eaglecrazy <munoyoo3munoyoo3>
2020-07-22 12:33:36 +0300

    Перенёс вывод регионов в _list.blade.php

--- resources/views/admin/regions/index.blade.php
+++ resources/views/admin/regions/index.blade.php
@@ -6,23 +6,5 @@
     <div class="d-flex flex-row mb-3">
         <a href="{{ route('admin.regions.create') }}" class="btn btn-primary mr-1">Create</a>
     </div>
-
-    <table class="table table-bordered table-striped">
-        <thead>
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Slug</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($regions as $region)
-            <tr>
-                <td>{{ $region->id }}</td>
-                <td><a href="{{ route('admin.regions.show', $region) }}">{{ $region->name }}</a></td>
-                <td>{{ $region->slug }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+    @include('admin.regions._list', ['regions' => $regions])
 @endsection
--- resources/views/admin/regions/show.blade.php
+++ resources/views/admin/regions/show.blade.php
@@ -35,23 +35,6 @@
         </tbody>
     </table>
     @if(count($region->children))
-        <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Slug</th>
-             </tr>
-         </thead>
-    <tbody>
-    @foreach($regions as $region)
-        <tr>
-            <td>{{ $region->id }}</td>
-            <td><a href="{{ route('admin.regions.show', $region) }}">{{ $region->name }}</a></td>
-            <td>{{ $region->slug }}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
-@endif
+        @include('admin.regions._list', ['regions' => $regions])
+    @endif
 @endsection
