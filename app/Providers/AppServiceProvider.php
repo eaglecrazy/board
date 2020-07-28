cd332be6ad329c0614674380f6aa5ddc9aaa14c9
eaglecrazy <munoyoo3munoyoo3>
2020-07-28 19:35:36 +0300

    Зарегистрировал смс сервис

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Services\Sms\SmsRu;
+use App\Services\Sms\SmsSender;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(SmsSender::class, function(Application $app){
+            $config = $app->make('config')->get('sms');
+            if(!empty($config['url'])){
+                return new SmsRu($config['api_id'], $config['url']);
+            }
+            return new SmsRu($config['api_id']);
+        });
     }
 
     /**
