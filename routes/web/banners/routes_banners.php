d424b19b267dfbf2c849c1517ea596373d90e23b
eaglezzzzz <eaglecrazy@mail.ru>
2021-01-26 13:21:12 +0300

    Убираю баги с показом баннеров

--- routes/web/banners/routes_banners.php
+++ routes/web/banners/routes_banners.php
@@ -4,4 +4,4 @@
 //---------
 
 Route::get('/bitem/get', 'BannerController@get')->name('banner.get');
-Route::get('/bitem/{banner}/click', 'BannerController@click')->name('banner.click');
+Route::get('/bitem/{banner}/go', 'BannerController@click')->name('banner.click');
