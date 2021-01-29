2359d61f56cc0d8fefcdb672efc2ca1ddd09af0c
eaglezzzzz <eaglecrazy@mail.ru>
2021-01-29 16:21:45 +0300

    Добавил вывод кода верификации телефона.

--- resources/views/auth/phone.blade.php
+++ resources/views/auth/phone.blade.php
@@ -7,7 +7,7 @@
     <form method="POST" action="{{ route('login.phone') }}">
         @csrf
         <div class="form-group">
-            <label for="token" class="col-form-label">SMS код <b style="color: red">Если код не пришёл то введите: {{ session()->get('auth.token') }}</b></label>
+            <label for="token" class="col-form-label">SMS код <br><b style="color: red">Если код не пришёл то введите: {{ session()->get('auth.token') }}</b></label>
             <input id="token" class="form-control{{ $errors->has('token') ? ' is-invalid' : '' }}" name="token" value="{{ old('token') }}" required>
             @if ($errors->has('token'))
                 <span class="invalid-feedback"><strong>{{ $errors->first('token') }}</strong></span>
--- resources/views/cabinet/profile/phone.blade.php
+++ resources/views/cabinet/profile/phone.blade.php
@@ -11,7 +11,7 @@
         @method('PUT')
 
         <div class="form-group">
-            <label for="token" class="col-form-label">SMS код <b style="color: red">Если код не пришёл то введите: {{ session()->get('auth.token') }}</b></label>
+            <label for="token" class="col-form-label">SMS код <br><b style="color: red">Если код не пришёл то введите: {{ Auth::user()->phone_verify_token }}</b></label>
             <input id="token" class="form-control{{ $errors->has('token') ? ' is-invalid' : '' }}" name="token" value="{{ old('token') }}" required>
             @if ($errors->has('token'))
                 <span class="invalid-feedback"><strong>{{ $errors->first('token') }}</strong></span>
