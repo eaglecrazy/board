161dcddd6c7e71b1d72123202ec5332a102b6f7f
eaglezzzzz <eaglecrazy@mail.ru>
2021-01-29 16:16:39 +0300

    Добавил вывод кода верификации телефона.

--- resources/views/cabinet/profile/phone.blade.php
+++ resources/views/cabinet/profile/phone.blade.php
@@ -4,14 +4,14 @@
 @section('content')
     @include('cabinet._nav', ['page' => 'profile'])
 
-    @dump(Auth::user()->phone_verify_token)
+{{--    @dump(Auth::user()->phone_verify_token)--}}
 
     <form method="POST" action="{{ route('cabinet.profile.phone.verify') }}">
         @csrf
         @method('PUT')
 
         <div class="form-group">
-            <label for="token" class="col-form-label">SMS код</label>
+            <label for="token" class="col-form-label">SMS код <b style="color: red">Если код не пришёл то введите: {{ session()->get('auth.token') }}</b></label>
             <input id="token" class="form-control{{ $errors->has('token') ? ' is-invalid' : '' }}" name="token" value="{{ old('token') }}" required>
             @if ($errors->has('token'))
                 <span class="invalid-feedback"><strong>{{ $errors->first('token') }}</strong></span>
