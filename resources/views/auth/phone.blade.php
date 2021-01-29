2b006c4e8b0c7bd2a3adfcc60d800a0d667fba6e
eaglezzzzz <eaglecrazy@mail.ru>
2021-01-29 16:11:23 +0300

    Добавил вывод кода верификации телефона.

--- resources/views/auth/phone.blade.php
+++ resources/views/auth/phone.blade.php
@@ -3,11 +3,11 @@
 
 @section('content')
 
-    @dump(session()->get('auth.token'))
+{{--    @dump(session()->get('auth.token'))--}}
     <form method="POST" action="{{ route('login.phone') }}">
         @csrf
         <div class="form-group">
-            <label for="token" class="col-form-label">SMS код</label>
+            <label for="token" class="col-form-label">SMS код <b style="color: red">Если код не пришёл то введите: {{ session()->get('auth.token') }}</b></label>
             <input id="token" class="form-control{{ $errors->has('token') ? ' is-invalid' : '' }}" name="token" value="{{ old('token') }}" required>
             @if ($errors->has('token'))
                 <span class="invalid-feedback"><strong>{{ $errors->first('token') }}</strong></span>
