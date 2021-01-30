08fac4b0c7d8ee356ab6569874c35ebbadd54771
eaglezzzzz <eaglecrazy@mail.ru>
2021-01-30 20:35:02 +0300

    Исправил отображение просмотра хидера

--- resources/views/layouts/partials/search.blade.php
+++ resources/views/layouts/partials/search.blade.php
@@ -1,69 +1,67 @@
 <div class="search-bar pt-3">
     <div class="container">
-        <div class="row">
-            <div class="col-md-9">
-                <form action="{{ route('adverts.index', adPath(isset($currentRegion) ? $currentRegion : null, isset($currentCategory) ? $currentCategory : null)) }}" method="GET">
-                    <div class="row">
-                        <div class="col-md-11">
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="text" value="{{ request('text') }}"
-                                       placeholder="Поиск ...">
-                            </div>
-                        </div>
-                        <div class="col-md-1">
-                            <div class="form-group">
-                                <button class="btn btn-light border" type="submit"><span class="fa fa-search"></span>
-                                </button>
-                            </div>
-                        </div>
+        <form
+            action="{{ route('adverts.index', adPath(isset($currentRegion) ? $currentRegion : null, isset($currentCategory) ? $currentCategory : null)) }}"
+            method="GET">
+            <div>
+                <div class="d-flex flex-fill flex-wrap">
+                    <div class="form-group mr-3 flex-fill">
+                        <input type="text" class="form-control" name="text" value="{{ request('text') }}"
+                               placeholder="Поиск ...">
                     </div>
-                    @if (isset($searchAttributes))
-                        <div class="row">
-                            @foreach ($searchAttributes as $attribute)
-                            @if ($attribute->isSelect() || $attribute->isNumber())
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label class="col-form-label">{{ $attribute->name }}</label>
+                    <div class="form-group mr-3">
+                        <button class="btn btn-light border" type="submit"><span class="fa fa-search"></span></button>
+                    </div>
+                    <div class="mb-3">
+                        <a href="{{ route('cabinet.adverts.create.category') }}" class="btn btn-success"><span
+                                class="fa fa-plus"></span> Добавить объявление</a>
+                    </div>
+                </div>
+            </div>
 
-                                            @if ($attribute->isSelect())
-                                                <select class="form-control" name="attrs[{{ $attribute->id }}][equals]">
-                                                    <option value=""></option>
-                                                    @foreach ($attribute->variants as $variant)
-                                                        <option
-                                                            value="{{ $variant }}"{{ $variant === request()->input('attrs.' . $attribute->id . '.equals') ? ' selected' : '' }}>
-                                                            {{ $variant }}
-                                                        </option>
-                                                    @endforeach
-                                                </select>
-                                            {{--можно выбрать диапазон значений, от и до--}}
-                                            @elseif ($attribute->isNumber())
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <input type="number" class="form-control"
-                                                               name="attrs[{{ $attribute->id }}][from]"
-                                                               value="{{ request()->input('attrs.' . $attribute->id . '.from') }}"
-                                                               placeholder="From">
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <input type="number" class="form-control"
-                                                               name="attrs[{{ $attribute->id }}][to]"
-                                                               value="{{ request()->input('attrs.' . $attribute->id . '.to') }}"
-                                                               placeholder="To">
-                                                    </div>
+            @if (isset($searchAttributes))
+                <div class="pb-2 negative-margin">
+                    <div class="d-flex flex-wrap">
+                        @foreach ($searchAttributes as $attribute)
+                            @if ($attribute->isSelect() || $attribute->isNumber())
+                                <div class="col-md-3">
+                                    <div class="form-group">
+                                        <label class="col-form-label">{{ $attribute->name }}</label>
+
+                                        @if ($attribute->isSelect())
+                                            <select class="form-control" name="attrs[{{ $attribute->id }}][equals]">
+                                                <option value=""></option>
+                                                @foreach ($attribute->variants as $variant)
+                                                    <option
+                                                        value="{{ $variant }}"{{ $variant === request()->input('attrs.' . $attribute->id . '.equals') ? ' selected' : '' }}>
+                                                        {{ $variant }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                            {{--                                                можно выбрать диапазон значений, от и до--}}
+                                        @elseif ($attribute->isNumber())
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <input type="number" class="form-control"
+                                                           name="attrs[{{ $attribute->id }}][from]"
+                                                           value="{{ request()->input('attrs.' . $attribute->id . '.from') }}"
+                                                           placeholder="From">
                                                 </div>
-                                            @endif
-                                        </div>
+                                                <div class="col-md-3">
+                                                    <input type="number" class="form-control"
+                                                           name="attrs[{{ $attribute->id }}][to]"
+                                                           value="{{ request()->input('attrs.' . $attribute->id . '.to') }}"
+                                                           placeholder="To">
+                                                </div>
+                                            </div>
+                                        @endif
                                     </div>
-                                @endif
-                            @endforeach
-                        </div>
-                    @endif
-                </form>
-            </div>
-            <div class="col-md-3" style="text-align: right">
-                <p><a href="{{ route('cabinet.adverts.create.category') }}" class="btn btn-success"><span
-                            class="fa fa-plus"></span>   Добавить объявление</a></p>
-            </div>
-        </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+        </form>
     </div>
 </div>
