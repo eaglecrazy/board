9b9361d01b0f8fe3b3ec53316496af79663a0d3f
eaglezzzzz <eaglecrazy@mail.ru>
2021-01-30 12:16:41 +0300

    Перевёл страницу отклонения объявления

--- resources/views/admin/adverts/reject.blade.php
+++ resources/views/admin/adverts/reject.blade.php
@@ -6,14 +6,14 @@
     <form method="POST" action="{{ route('admin.adverts.adverts.refuse', $advert) }}">
         @csrf
         <div class="form-group">
-            <label for="reason" class="col-form-label">Reason</label>
+            <label for="reason" class="col-form-label">Причина отклонения</label>
             <textarea id="reason" class="form-control{{ $errors->has('reason') ? ' is-invalid' : '' }}" name="reason" rows="10" required>{{ old('reason', $advert->reject_reason) }}</textarea>
             @if ($errors->has('reason'))
                 <span class="invalid-feedback"><strong>{{ $errors->first('reason') }}</strong></span>
             @endif
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-primary">Reject</button>
+            <button type="submit" class="btn btn-primary">Отклонить</button>
         </div>
     </form>
 @endsection
