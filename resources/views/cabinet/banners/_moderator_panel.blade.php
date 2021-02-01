04a44993507ebe2ac5c3f9702bc984ae1441dd3a
eaglezzzzz <eaglecrazy@mail.ru>
2021-02-01 11:53:45 +0300

    Исправил отображение панелей управления баннерами.

--- resources/views/cabinet/banners/_moderator_panel.blade.php
+++ resources/views/cabinet/banners/_moderator_panel.blade.php
@@ -1,21 +1,21 @@
 <div class="card card-default mb-2">
     <div class="card-header h4">Панель модератора</div>
-    <div class="card-body d-flex flex-row">
-        <a href="{{ route('admin.banners.edit', $banner) }}" class="btn btn-primary mr-1">Редактировать</a>
+    <div class="card-body d-flex flex-wrap p-1">
+        <a href="{{ route('admin.banners.edit', $banner) }}" class="btn btn-primary m-1">Редактировать</a>
         @if ($banner->isOnModeration())
-            <form method="POST" action="{{ route('admin.banners.moderate', $banner) }}" class="mr-1">
+            <form method="POST" action="{{ route('admin.banners.moderate', $banner) }}" class="m-1">
                 @csrf
                 <button class="btn btn-success">Одобрить</button>
             </form>
-            <a href="{{ route('admin.banners.reject', $banner) }}" class="btn btn-warning mr-1">Отклонить</a>
+            <a href="{{ route('admin.banners.reject', $banner) }}" class="btn btn-warning m-1">Отклонить</a>
         @endif
         @if ($banner->isOrdered())
-            <form method="POST" action="{{ route('admin.banners.pay', $banner) }}" class="mr-1">
+            <form method="POST" action="{{ route('admin.banners.pay', $banner) }}" class="m-1">
                 @csrf
                 <button class="btn btn-success">Отметить как оплаченный</button>
             </form>
         @endif
-        <form method="POST" action="{{ route('admin.banners.destroy', $banner) }}" class="mr-1">
+        <form method="POST" action="{{ route('admin.banners.destroy', $banner) }}" class="m-1">
             @csrf
             @method('DELETE')
             <button class="btn btn-danger">Удалить</button>
--- resources/views/cabinet/banners/_owner_panel.blade.php
+++ resources/views/cabinet/banners/_owner_panel.blade.php
@@ -1,31 +1,31 @@
 <div class="card card-default mb-3">
     <div class="card-header h4">Управление баннером</div>
-    <div class="card-body d-flex flex-row">
+    <div class="card-body d-flex flex-wrap p-1">
         @if ($banner->canBeChanged())
-            <a href="{{ route('cabinet.banners.edit', $banner) }}" class="btn btn-primary mr-1">Редактировать</a>
-            <a href="{{ route('cabinet.banners.edit_file', $banner) }}" class="btn btn-primary mr-1">Изменить
+            <a href="{{ route('cabinet.banners.edit', $banner) }}" class="btn btn-primary m-1">Редактировать</a>
+            <a href="{{ route('cabinet.banners.edit_file', $banner) }}" class="btn btn-primary m-1">Изменить
                 файл</a>
         @endif
         @if ($banner->isDraft())
-            <form method="POST" action="{{ route('cabinet.banners.sendToModeration', $banner) }}" class="mr-1">
+            <form method="POST" action="{{ route('cabinet.banners.sendToModeration', $banner) }}" class="m-1">
                 @csrf
                 <button class="btn btn-success">Отправить на модерацию</button>
             </form>
         @endif
         @if ($banner->isOnModeration())
-            <form method="POST" action="{{ route('cabinet.banners.cancelModeration', $banner) }}" class="mr-1">
+            <form method="POST" action="{{ route('cabinet.banners.cancelModeration', $banner) }}" class="m-1">
                 @csrf
                 <button class="btn btn-secondary">Отозвать с модерации</button>
             </form>
         @endif
         @if ($banner->isModerated())
-            <form method="POST" action="{{ route('cabinet.banners.order', $banner) }}" class="mr-1">
+            <form method="POST" action="{{ route('cabinet.banners.order', $banner) }}" class="m-1">
                 @csrf
                 <button class="btn btn-success">Оплатить</button>
             </form>
         @endif
         @if ($banner->canBeRemoved())
-            <form method="POST" action="{{ route('cabinet.banners.destroy', $banner) }}" class="mr-1">
+            <form method="POST" action="{{ route('cabinet.banners.destroy', $banner) }}" class="m-1">
                 @csrf
                 @method('DELETE')
                 <button class="btn btn-danger">Удалить</button>
